2a0a4f63753c1c79005912ecab48ea492f923edc
gnanesmk <gnaneshwer.mk@sscinc.com>
2025-06-09 18:34:43 +0530

    use env

--- client/src/helper/data.ts
+++ client/src/helper/data.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
-
+console.log(import.meta.env.VITE_PROD_API_URL)
 const instance = axios.create({
-    baseURL: import.meta.env.VITE_PROD_API_URL || 'http://localhost:5000/api',
+    baseURL: import.meta.env.VITE_PROD_API_URL,
     headers:{
         'Content-Type':'application/json'
     }
